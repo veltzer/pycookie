604a4ccc7ee475e2db91a84bd81018b98d285d66
Mark Veltzer <mark.veltzer@gmail.com>
2021-05-21 05:12:02 +0300

    some code

--- pycookie/main.py
+++ pycookie/main.py
@@ -7,6 +7,28 @@ import pylogconf.core
 import tqdm
 from pytconf import register_main, config_arg_parse_and_launch, register_endpoint
 from pycookie.static import VERSION_STR
+import browsercookie
+
+
+@register_endpoint(
+    description="dump cookies",
+    configs=[
+    ],
+)
+def dump_cookies() -> None:
+    # cj = browsercookie.load()
+    # cj = browsercookie.chrome()
+    cj = browsercookie.firefox()
+    # print(len(cj._cookies))
+    # print(cj._cookies)
+    # noinspection PyProtectedMember
+    for k, v in cj._cookies.items():
+        print("=======================")
+        print(k, v)
+    # print(dir(cj))
+    # help(cj.make_cookies)
+    # help(cj.extract_cookies)
+
 
 @register_main(
     main_description="pycookie will help you remove duplicate files",
