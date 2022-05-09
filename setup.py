d2165f90022a5dd0d9158d63c8ba5b5f94e59e36
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-09 13:10:52 +0300

    new version of python

--- setup.py
+++ setup.py
@@ -33,6 +33,11 @@ setuptools.setup(
     platforms=[
         'python3',
     ],
+    install_requires=[
+        'pytconf',
+        'pylogconf',
+        'browsercookie',
+    ],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
@@ -49,5 +54,5 @@ setuptools.setup(
     entry_points={"console_scripts": [
         'pycookie=pycookie.main:main',
     ]},
-    python_requires=">=3.9",
+    python_requires=">=3.10",
 )
