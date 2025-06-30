25e9afc271981b9586388b26ed4378eb3f2a6539
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-30 14:10:00 +0300

    fixed up deps

--- config/python.py
+++ config/python.py
@@ -1,28 +1,17 @@
 """ python deps for this project """
 
+import config.shared
+
 scripts: dict[str,str] = {
     "pycookie": "pycookie.main:main",
 }
-config_requires: list[str] = [
-    "pyclassifiers",
-]
+
 install_requires: list[str] = [
     "pytconf",
     "pylogconf",
     "browsercookie",
-]
-build_requires: list[str] = [
-    "hatch",
-    "pydmt",
-    "pymakehelper",
-    "pycmdtools",
-    # modules
     "pyyaml",
 ]
-test_requires: list[str] = [
-    "pylint",
-    "pytest",
-    "mypy",
-    "ruff",
-]
-requires = config_requires + install_requires + build_requires + test_requires
+build_requires: list[str] = config.shared.PBUILD
+test_requires: list[str] = config.shared.PTEST
+requires = install_requires + build_requires + test_requires
