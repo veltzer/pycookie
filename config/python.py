ed895d42eb0fd9e48bbd7f1b0906408aa64bd55e
Mark Veltzer <mark.veltzer@gmail.com>
2025-07-01 15:55:14 +0300

    better reqs

--- config/python.py
+++ config/python.py
@@ -2,10 +2,6 @@
 
 import config.shared
 
-scripts: dict[str,str] = {
-    "pycookie": "pycookie.main:main",
-}
-
 install_requires: list[str] = [
     "pytconf",
     "pylogconf",
@@ -15,3 +11,7 @@ install_requires: list[str] = [
 build_requires: list[str] = config.shared.PBUILD
 test_requires: list[str] = config.shared.PTEST
 requires = install_requires + build_requires + test_requires
+
+scripts: dict[str,str] = {
+    "pycookie": "pycookie.main:main",
+}
